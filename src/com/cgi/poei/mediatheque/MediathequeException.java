16096d856cae725e6df356a7a049f5f02c9342be
David <dagaydevel@free.fr>
2018-05-07 10:02:07 +0200

    suppression warning

--- src/com/cgi/poei/mediatheque/MediathequeException.java
+++ src/com/cgi/poei/mediatheque/MediathequeException.java
@@ -2,6 +2,8 @@ package com.cgi.poei.mediatheque;
 
 public class MediathequeException extends Exception {
 	
+	private static final long serialVersionUID = 1L;
+
 	public MediathequeException(String message) {
 		super(message);
 	}
